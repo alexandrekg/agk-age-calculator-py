81934f77c4508a53bf00567f039f57c041f6386a
Alexandre K. Giacobo <alexandrekgiacobo@gmail.com>
2024-11-20 11:48:39 -0300

    Finalizando projeto

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def main():
+    birth_year = int(input("Enter your birth year: "))
+    current_year = int(input("Enter the current year: "))
+    result = current_year - birth_year
+    print(f"You are {result} years old.")
+
+
+if __name__ == "__main__":
+    main()
